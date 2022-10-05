71283296409d8698b09952def9ba12432b98a894
ggtejaswini <102533188+ggtejaswini@users.noreply.github.com>
2022-10-05 12:56:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,614 @@
+function setup() {
+    canvas = createCanvas(640, 480);
+    canvas.position(110, 250);
+    video = createCapture(VIDEO);
+    video.hide();
+
+}
+
+function draw() {
+    image(video,100,80,350,250);
+    stroke(189,50,40)
+    circle(40,40,50)
+
+    stroke(224, 161, 13)
+    circle(50,40,50)
+
+    stroke(245, 245, 20)
+    circle(60,40,50)
+
+    stroke(108, 222, 16)
+    circle(70,40,50)
+
+    stroke(45, 94, 5)
+    circle(80,40,50)
+
+    stroke(28, 217, 154)
+    circle(90,40,50)
+
+     stroke(7, 70, 102)
+    circle(100,40,50)
+    stroke(189,50,40)
+    circle(110,40,50)
+
+    stroke(224, 161, 13)
+    circle(120,40,50)
+
+    stroke(245, 245, 20)
+    circle(130,40,50)
+
+    stroke(108, 222, 16)
+    circle(140,40,50)
+
+    stroke(45, 94, 5)
+    circle(150,40,50)
+
+    stroke(28, 217, 154)
+    circle(160,40,50)
+
+     stroke(7, 70, 102)
+    circle(170,40,50)
+    stroke(189,50,40)
+    circle(180,40,50)
+
+    stroke(224, 161, 13)
+    circle(190,40,50)
+
+    stroke(245, 245, 20)
+    circle(200,40,50)
+
+    stroke(108, 222, 16)
+    circle(210,40,50)
+
+    stroke(45, 94, 5)
+    circle(220,40,50)
+
+    stroke(28, 217, 154)
+    circle(230,40,50)
+
+     stroke(7, 70, 102)
+    circle(240,40,50)
+    stroke(189,50,40)
+    circle(250,40,50)
+
+    stroke(224, 161, 13)
+    circle(260,40,50)
+
+    stroke(245, 245, 20)
+    circle(270,40,50)
+
+    stroke(108, 222, 16)
+    circle(280,40,50)
+
+    stroke(45, 94, 5)
+    circle(290,40,50)
+
+    stroke(28, 217, 154)
+    circle(300,40,50)
+
+     stroke(7, 70, 102)
+    circle(310,40,50)
+    stroke(189,50,40)
+    circle(320,40,50)
+
+    stroke(224, 161, 13)
+    circle(330,40,50)
+
+    stroke(245, 245, 20)
+    circle(340,40,50)
+
+    stroke(108, 222, 16)
+    circle(350,40,50)
+
+    stroke(45, 94, 5)
+    circle(360,40,50)
+
+    stroke(28, 217, 154)
+    circle(370,40,50)
+
+     stroke(7, 70, 102)
+    circle(380,40,50)
+    stroke(189,50,40)
+    circle(390,40,50)
+
+    stroke(224, 161, 13)
+    circle(400,40,50)
+
+    stroke(245, 245, 20)
+    circle(410,40,50)
+
+    stroke(108, 222, 16)
+    circle(420,40,50)
+
+    stroke(45, 94, 5)
+    circle(430,40,50)
+
+    stroke(28, 217, 154)
+    circle(440,40,50)
+
+     stroke(7, 70, 102)
+    circle(450,40,50)
+
+    stroke(189,50,40)
+    circle(460,40,50)
+
+    stroke(224, 161, 13)
+    circle(470,40,50)
+
+    stroke(245, 245, 20)
+    circle(480,40,50)
+
+    stroke(108, 222, 16)
+    circle(490,40,50)
+
+    stroke(45, 94, 5)
+    circle(500,40,50)
+
+    stroke(28, 217, 154)
+    circle(510,40,50)
+
+     stroke(7, 70, 102)
+    circle(520,40,50)
+    stroke(224, 161, 13)
+    circle(530,40,50)
+
+    stroke(245, 245, 20)
+    circle(540,40,50)
+
+    stroke(108, 222, 16)
+    circle(550,40,50)
+
+    stroke(45, 94, 5)
+    circle(560,40,50)
+
+   
+
+
+
+    stroke(224, 161, 13)
+    circle(40,50,50)
+
+    stroke(245, 245, 20)
+    circle(40,60,50)
+
+    stroke(108, 222, 16)
+    circle(40,70,50)
+
+    stroke(45, 94, 5)
+    circle(40,80,50)
+
+    stroke(28, 217, 154)
+    circle(40,90,50)
+
+     stroke(7, 70, 102)
+    circle(40,100,50)
+
+    stroke(224, 161, 13)
+    circle(40,110,50)
+
+    stroke(245, 245, 20)
+    circle(40,120,50)
+
+    stroke(108, 222, 16)
+    circle(40,130,50)
+
+    stroke(45, 94, 5)
+    circle(40,140,50)
+
+    stroke(28, 217, 154)
+    circle(40,150,50)
+
+     stroke(7, 70, 102)
+    circle(40,160,50)
+
+    
+    stroke(224, 161, 13)
+    circle(40,170,50)
+
+    stroke(245, 245, 20)
+    circle(40,180,50)
+
+    stroke(108, 222, 16)
+    circle(40,190,50)
+
+    stroke(45, 94, 5)
+    circle(40,200,50)
+
+    stroke(28, 217, 154)
+    circle(40,210,50)
+
+     stroke(7, 70, 102)
+    circle(40,220,50)
+
+    stroke(224, 161, 13)
+    circle(40,230,50)
+
+    stroke(245, 245, 20)
+    circle(40,240,50)
+
+    stroke(108, 222, 16)
+    circle(40,250,50)
+
+    stroke(45, 94, 5)
+    circle(40,260,50)
+
+    stroke(28, 217, 154)
+    circle(40,270,50)
+
+     stroke(7, 70, 102)
+    circle(40,280,50)
+
+    stroke(224, 161, 13)
+    circle(40,290,50)
+
+    stroke(245, 245, 20)
+    circle(40,300,50)
+
+    stroke(108, 222, 16)
+    circle(40,310,50)
+
+    stroke(45, 94, 5)
+    circle(40,320,50)
+
+    stroke(28, 217, 154)
+    circle(40,330,50)
+
+     stroke(7, 70, 102)
+    circle(40,340,50)
+
+
+
+    stroke(224, 161, 13)
+    circle(40,50,50)
+
+    stroke(245, 245, 20)
+    circle(40,60,50)
+
+    stroke(108, 222, 16)
+    circle(40,70,50)
+
+    stroke(45, 94, 5)
+    circle(40,80,50)
+
+    stroke(28, 217, 154)
+    circle(40,90,50)
+
+     stroke(7, 70, 102)
+    circle(40,100,50)
+
+    stroke(224, 161, 13)
+    circle(40,110,50)
+
+    stroke(245, 245, 20)
+    circle(40,120,50)
+
+    stroke(108, 222, 16)
+    circle(40,130,50)
+
+    stroke(45, 94, 5)
+    circle(40,140,50)
+
+    stroke(28, 217, 154)
+    circle(40,150,50)
+
+     stroke(7, 70, 102)
+    circle(40,160,50)
+
+    
+    stroke(224, 161, 13)
+    circle(40,170,50)
+
+    stroke(245, 245, 20)
+    circle(40,180,50)
+
+    stroke(108, 222, 16)
+    circle(40,190,50)
+
+    stroke(45, 94, 5)
+    circle(40,200,50)
+
+    stroke(28, 217, 154)
+    circle(40,210,50)
+
+     stroke(7, 70, 102)
+    circle(40,220,50)
+
+    stroke(224, 161, 13)
+    circle(40,230,50)
+
+    stroke(245, 245, 20)
+    circle(40,240,50)
+
+    stroke(108, 222, 16)
+    circle(40,250,50)
+
+    stroke(45, 94, 5)
+    circle(40,260,50)
+
+    stroke(28, 217, 154)
+    circle(40,270,50)
+
+     stroke(7, 70, 102)
+    circle(40,280,50)
+
+    stroke(224, 161, 13)
+    circle(40,290,50)
+
+    stroke(245, 245, 20)
+    circle(40,300,50)
+
+    stroke(108, 222, 16)
+    circle(40,310,50)
+
+    stroke(45, 94, 5)
+    circle(40,320,50)
+
+    stroke(28, 217, 154)
+    circle(40,330,50)
+
+     stroke(7, 70, 102)
+    circle(40,340,50)
+
+    
+    stroke(7, 70, 102)
+    circle(570,40,50)
+   
+    stroke(7, 70, 102)
+    circle(570,50,50)
+    
+    stroke(224, 161, 13)
+    circle(570,60,50)
+
+    stroke(245, 245, 20)
+    circle(570,70,50)
+
+    stroke(108, 222, 16)
+    circle(570,80,50)
+
+    stroke(45, 94, 5)
+    circle(570,90,50)
+
+    stroke(28, 217, 154)
+    circle(570,100,50)
+
+     stroke(7, 70, 102)
+    circle(570,110,50)
+
+    stroke(224, 161, 13)
+    circle(570,120,50)
+
+    stroke(245, 245, 20)
+    circle(570,130,50)
+
+    stroke(108, 222, 16)
+    circle(570,140,50)
+
+    stroke(45, 94, 5)
+    circle(570,150,50)
+
+    stroke(28, 217, 154)
+    circle(570,160,50)
+
+     stroke(7, 70, 102)
+    circle(570,170,50)
+
+    stroke(224, 161, 13)
+    circle(570,180,50)
+
+    stroke(245, 245, 20)
+    circle(570,190,50)
+
+    stroke(108, 222, 16)
+    circle(570,200,50)
+
+    stroke(45, 94, 5)
+    circle(570,210,50)
+
+    stroke(28, 217, 154)
+    circle(570,220,50)
+
+     stroke(7, 70, 102)
+    circle(570,230,50)
+
+    stroke(7, 70, 102)
+    circle(570,240,50)
+   
+    stroke(7, 70, 102)
+    circle(570,250,50)
+    
+    stroke(224, 161, 13)
+    circle(570,260,50)
+
+    stroke(245, 245, 20)
+    circle(570,270,50)
+
+    stroke(108, 222, 16)
+    circle(570,280,50)
+
+    stroke(45, 94, 5)
+    circle(570,290,50)
+
+    stroke(28, 217, 154)
+    circle(570,300,50)
+
+     stroke(7, 70, 102)
+    circle(570,310,50)
+
+    stroke(224, 161, 13)
+    circle(570,320,50)
+
+    stroke(245, 245, 20)
+    circle(570,330,50)
+
+    stroke(108, 222, 16)
+    circle(570,340,50)
+
+    stroke(45, 94, 5)
+    circle(570,350,50)
+
+    
+
+    stroke(45, 94, 5)
+    circle(560,350,50)
+      
+    stroke(28, 217, 154)
+    circle(550,350,50)
+
+     stroke(7, 70, 102)
+    circle(540,350,50)
+
+    stroke(7, 70, 102)
+    circle(530,350,50)
+   
+    stroke(7, 70, 102)
+    circle(520,350,50)
+    
+    stroke(224, 161, 13)
+    circle(510,350,50)
+
+    stroke(245, 245, 20)
+    circle(500,350,50)
+
+    stroke(108, 222, 16)
+    circle(490,350,50)
+
+    stroke(45, 94, 5)
+    circle(480,350,50)
+
+    stroke(28, 217, 154)
+    circle(470,350,50)
+
+    stroke(28, 217, 154)
+    circle(460,350,50)
+
+     stroke(7, 70, 102)
+    circle(450,350,50)
+
+    stroke(7, 70, 102)
+    circle(440,350,50)
+   
+    stroke(7, 70, 102)
+    circle(430,350,50)
+    
+    stroke(224, 161, 13)
+    circle(420,350,50)
+
+    stroke(245, 245, 20)
+    circle(410,350,50)
+
+    stroke(108, 222, 16)
+    circle(400,350,50)
+
+    stroke(45, 94, 5)
+    circle(390,350,50)
+
+    stroke(28, 217, 154)
+    circle(380,350,50)
+
+    stroke(45, 94, 5)
+    circle(370,350,50)
+      
+    stroke(28, 217, 154)
+    circle(360,350,50)
+
+     stroke(7, 70, 102)
+    circle(350,350,50)
+
+    stroke(7, 70, 102)
+    circle(340,350,50)
+   
+    stroke(7, 70, 102)
+    circle(330,350,50)
+    
+    stroke(224, 161, 13)
+    circle(320,350,50)
+
+    stroke(245, 245, 20)
+    circle(310,350,50)
+
+    stroke(108, 222, 16)
+    circle(300,350,50)
+
+    stroke(45, 94, 5)
+    circle(290,350,50)
+
+    stroke(28, 217, 154)
+    circle(280,350,50)
+
+    stroke(28, 217, 154)
+    circle(270,350,50)
+
+     stroke(7, 70, 102)
+    circle(260,350,50)
+
+    stroke(7, 70, 102)
+    circle(250,350,50)
+   
+    stroke(7, 70, 102)
+    circle(240,350,50)
+    
+    stroke(224, 161, 13)
+    circle(230,350,50)
+
+    stroke(245, 245, 20)
+    circle(220,350,50)
+
+    stroke(108, 222, 16)
+    circle(210,350,50)
+
+    stroke(45, 94, 5)
+    circle(200,350,50)
+
+    stroke(28, 217, 154)
+    circle(190,350,50)
+    
+    stroke(108, 222, 16)
+    circle(180,350,50)
+
+    stroke(45, 94, 5)
+    circle(170,350,50)
+
+    stroke(28, 217, 154)
+    circle(160,350,50)
+
+    stroke(45, 94, 5)
+    circle(150,350,50)
+      
+    stroke(28, 217, 154)
+    circle(140,350,50)
+
+     stroke(7, 70, 102)
+    circle(130,350,50)
+
+    stroke(7, 70, 102)
+    circle(120,350,50)
+   
+    stroke(7, 70, 102)
+    circle(110,350,50)
+    
+    stroke(224, 161, 13)
+    circle(100,350,50)
+
+    stroke(245, 245, 20)
+    circle(90,350,50)
+
+    stroke(108, 222, 16)
+    circle(80,350,50)
+
+    stroke(45, 94, 5)
+    circle(70,350,50)
+
+    stroke(28, 217, 154)
+    circle(60,350,50)
+
+    stroke(7, 70, 102)
+    circle(50,350,50)
+
+    stroke(224, 161, 13)
+    circle(40,350,50)
+
+}
+
+function take_snapshot(){
+    save('frame_image.png')
+}
